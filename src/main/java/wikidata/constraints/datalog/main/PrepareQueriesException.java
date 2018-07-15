545b0e5ff323485049759abdd65edab9e3c054dc
Adrian Bielefeldt <Adrian.Bielefeldt@mailbox.tu-dresden.de>
2018-07-15 17:03:47 +0200

    FIX: Commit for forgotten exception

---
+++ src/main/java/wikidata/constraints/datalog/main/PrepareQueriesException.java
@@ -0,0 +1,9 @@
+package wikidata.constraints.datalog.main;
+
+public class PrepareQueriesException extends Exception {
+	
+	public PrepareQueriesException(String message) {
+		super(message);
+	}
+
+}
