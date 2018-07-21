3a21aa2d80ea10ced7af4113405e76df889d1b16
Adrian Bielefeldt <Adrian.Bielefeldt@mailbox.tu-dresden.de>
2018-07-21 13:38:05 +0200

    Started type constraint

---
+++ src/main/java/wikidata/constraints/datalog/impl/TS/ItemInstanceTS.java
@@ -0,0 +1,50 @@
+package wikidata.constraints.datalog.impl.TS;
+
+import java.io.IOException;
+
+import org.wikidata.wdtk.datamodel.interfaces.ItemDocument;
+import org.wikidata.wdtk.datamodel.interfaces.Statement;
+import org.wikidata.wdtk.datamodel.interfaces.StatementGroup;
+
+import wikidata.constraints.datalog.utility.OutputValueVisitor;
+
+public class ItemInstanceTS extends TripleSet {
+
+	public ItemInstanceTS(String property_) throws IOException {
+		super(property_);
+	}
+	
+	@Override
+	public void processItemDocument(ItemDocument itemDocument) {
+		boolean foundProperty = false;
+		
+		String subject = itemDocument.getEntityId().getIri();
+		for (StatementGroup sg : itemDocument.getStatementGroups()) {
+			if (foundProperty)
+				break;
+			String predicate = sg.getProperty().getIri();
+			if (predicate.endsWith(property)) {
+				foundProperty = true;
+				break;
+			}
+		}
+		if (!foundProperty)
+			return;
+		for (StatementGroup	sg : itemDocument.getStatementGroups()) {
+			String predicate = sg.getProperty().getIri();
+			if (!predicate.endsWith("P31"))
+				return;
+			for (Statement statement : sg) {
+				String id = statement.getStatementId();
+				String object = statement.getValue().accept(new OutputValueVisitor());
+				triple(id, subject, predicate, object);
+			}
+		}
+	}
+
+	@Override
+	protected String getTripleSetType() {
+		return "";
+	}
+
+}
